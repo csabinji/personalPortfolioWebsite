a20ab16abf08a382e185c4880ce8e90d41f40972
sabin <csabinji@gmail.com>
2021-12-29 16:35:10 +0545

    about me

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './assets/css/style.css'
 import Header from './components/header/header'
+import About from "./components/about/about";
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
+     <Header/>
+     <About/>
     </div>
   );
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,21 @@
+import './about.css'
+
+export default function About() {
+    return (
+        <section id="about" class="about">
+            <div class="container">
+
+                <div class="section-title">
+                    <span>About Me</span>
+                    <h2>About Me</h2>
+                    <p>Sabin Chapagain (born October 25, 1998), is a Nepalese rapper, songwriter and beat producer.
+                        He was born in Damak, Jhapa. He started making music in 2015 with his debut mixtape "Sangarsha".
+                        He moved to Kathmandu in 2018 to attend his college and continue to make Hiphop music.
+                        He grew up listening to Yama Buddha, Mc flo and Girish Khatiwada. </p>
+                    <br />
+                    <p>He recently started his own recording studio where any new talent is welcomed.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
