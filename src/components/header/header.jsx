c5482440d2860c1d319d55aaabaca3b02214f399
sabin <csabinji@gmail.com>
2021-12-31 13:56:19 +0545

    Toggle .header-scrolled class to #header when page is scrolled

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,10 +3,10 @@ import Navigation from "./navigation/navigation";
 import { useState } from "react";
 
 export default function Headder() {
-    const [showMenu, setMenuOpen] = useState(false)
+    const [showMenu, setMenuOpen, scroll, setScroll] = useState(false)
     return (
         <div className="App">
-            <Navigation showMenu={showMenu} setMenuOpen={setMenuOpen} />
+            <Navigation showMenu={showMenu} setMenuOpen={setMenuOpen} scroll={scroll} setScroll={setScroll} />
             <Hero />
         </div>
     )
--- src/components/header/navigation/navigation.jsx
+++ src/components/header/navigation/navigation.jsx
@@ -1,11 +1,28 @@
 import "./navigation.css"
 import { FiMenu } from 'react-icons/fi';
 import { MdCancel } from 'react-icons/md';
+import { useEffect, useState } from "react";
 
 export default function Navigation({ showMenu, setMenuOpen }) {
-    return (
-        <header id="header" class="fixed-top d-flex justify-content-center align-items-center header-transparent">
+    const [header, setHeader] = useState(false)
 
+    const listenScrollEvent = () => {
+        if (window.scrollY > 100) {
+            return setHeader(true)
+        } else {
+            return setHeader(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+
+        return () =>
+            window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
+
+    return (
+        <header id="header" className={`fixed-top d-flex justify-content-center align-items-center header-transparent ${header ? "header-scrolled" : ""}`}>
             <nav id="navbar" className={showMenu ? "navbar navbar-mobile" : "navbar"}>
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
